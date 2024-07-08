769dbf1272582e8c1f77fe5bc003cc65606065cb
Hyunji-coding <dnflsms3@naver.com>
2024-07-08 23:38:26 +0900

    feat : wishlistdto 구현

---
+++ src/main/java/org/example/login/dto/Request/WishListRequest.java
@@ -0,0 +1,14 @@
+package org.example.login.dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WishListRequest {
+    private Long productId;
+}
---
+++ src/main/java/org/example/login/dto/Response/WishListResponse.java
@@ -0,0 +1,28 @@
+package org.example.login.dto.Response;
+
+import lombok.Builder;
+import lombok.Data;
+import org.example.login.entity.ShoppingCart;
+import org.example.login.entity.WishList;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class WishListResponse {
+    private String productName;
+    private String category;
+    private String productImg;
+    private Long price;
+    private LocalDateTime createdAt;
+
+    public static WishListResponse fromEntity(WishList wishList) {
+        return WishListResponse.builder()
+                .productName(wishList.getProducts().getName())
+                .category(wishList.getProducts().getCategories().getName())
+                .createdAt(wishList.getCreatedAt())
+                .productImg(wishList.getProducts().getImg())
+                .price(wishList.getProducts().getPrice())
+                .build();
+    }
+}
