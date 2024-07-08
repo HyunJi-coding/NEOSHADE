0f26598af602437299fe9dc1dbdf4c4fdc40f6a1
Hyunji-coding <dnflsms3@naver.com>
2024-07-08 23:36:14 +0900

    feat : wishlistservice 메서드 구현

---
+++ src/main/java/org/example/login/service/WishListService.java
@@ -0,0 +1,59 @@
+package org.example.login.service;
+
+import org.example.login.dto.Request.WishListRequest;
+import org.example.login.dto.Response.WishListResponse;
+import org.example.login.entity.Products;
+import org.example.login.entity.Users;
+import org.example.login.entity.WishList;
+import org.example.login.repository.ProductsRepo;
+import org.example.login.repository.UsersRepo;
+import org.example.login.repository.WishListRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class WishListService {
+    private final WishListRepo wishListRepo;
+    private final ProductsRepo productsRepo;
+    private final UsersRepo usersRepo;
+
+    @Autowired
+    public WishListService(WishListRepo wishListRepo, ProductsRepo productsRepo, UsersRepo usersRepo) {
+        this.wishListRepo = wishListRepo;
+        this.productsRepo = productsRepo;
+        this.usersRepo = usersRepo;
+    }
+
+    public WishListResponse addWishList(Long userId, WishListRequest WishListRequest) {
+        Products product = productsRepo.findById(WishListRequest.getProductId()).get();
+
+        Users user = usersRepo.findById(userId).get();
+
+
+        WishList wishList = WishList.builder()
+                .users(user)
+                .products(product)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        wishList = wishListRepo.save(wishList);
+
+        return WishListResponse.fromEntity(wishList);
+    }
+
+    public List<WishListResponse> getWishList(Long userId) {
+        List<WishList> wishList = wishListRepo.findByUsersUserId(userId);
+        return wishList.stream()
+                .map(WishListResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public void removeWishList(Long userId, Long wishListItemId) {
+        WishList wishList = wishListRepo.findByWishlistIdAndUsersUserId(wishListItemId, userId);
+        wishListRepo.delete(wishList);
+    }
+}
