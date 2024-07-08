0824a00eada788d8ba3d44875e6cc0046ac657d7
Hyunji-coding <dnflsms3@naver.com>
2024-07-09 02:09:02 +0900

    feat : payment entity 및 dto 생성

---
+++ src/main/java/org/example/login/dto/Request/PaymentsRequest.java
@@ -0,0 +1,15 @@
+package org.example.login.dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentsRequest {
+    private String paymentMethod;
+    private double paymentAmount;
+}
---
+++ src/main/java/org/example/login/entity/Payments.java
@@ -0,0 +1,41 @@
+package org.example.login.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long paymentId;
+
+    @OneToOne
+    @JoinColumn(name = "orderId", referencedColumnName = "orderId")
+    private Orders order;
+
+    private String paymentMethod;
+    private String paymentStatus;
+    private double paymentAmount;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (paymentMethod == null) {
+            paymentMethod = "card";
+        }
+        if (paymentStatus == null) {
+            paymentStatus = "Complete";
+        }
+    }
+
+
+}
