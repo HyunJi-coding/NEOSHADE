8938230de89cc3cad573d1e4f2ce78077c417185
Hyunji-coding <dnflsms3@naver.com>
2024-07-17 23:41:50 +0900

    feat: 저장된 세션 불러오기

--- src/main/java/org/example/login/controller/AuthCon.java
+++ src/main/java/org/example/login/controller/AuthCon.java
@@ -6,11 +6,34 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+
 @RestController
 public class AuthCon {
-    @GetMapping("/api/check-auth")
+    @GetMapping("/check-auth")
     public boolean checkAuth() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken);
+    }
+
+    @GetMapping("/sessionInfo")
+    public String doSession(HttpServletRequest req){
+        String strReturn="";
+        HttpSession session = req.getSession();
+
+        String strSessionId = session.getId();
+        strReturn= "session ID : "+strSessionId+"<br>";
+
+        Enumeration<String> names = session.getAttributeNames();
+
+        String strSession="";
+        while(names.hasMoreElements()){
+            strSession = names.nextElement();
+            strReturn += strSession+" : "+ session.getAttribute(strSession) +"</br>";
+        }
+
+        return strReturn;
     }
 }
